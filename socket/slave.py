5633d6c7ad6f6d928fa220eb3be5a4dff5249a4b
Anshuman Dash <anshuman@ucsb.edu>
2022-11-12 16:49:20 -0800

    :)

---
+++ socket/slave.py
@@ -0,0 +1,31 @@
+# echo-client.py
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 20001  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
+
+import socket
+
+msgFromClient       = "Hello UDP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("127.0.0.1", 20001)
+bufferSize          = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
